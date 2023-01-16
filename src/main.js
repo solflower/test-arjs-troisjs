d618e5b0dd4451621b22e851cb6b83f7f0d13759
onrawin <onrawin.h@ku.th>
2023-01-16 22:25:31 +0700

    rearrange App.vue and delete webpack config

--- src/main.js
+++ src/main.js
@@ -2,8 +2,9 @@ import { createApp } from 'troisjs'
 import SignUp from './components/SignUp.vue'
 import Login from './components/Login.vue'
 import TestCube from './components/TestCube.vue'
+import App from './App.vue'
 
 import 'bootstrap/dist/css/bootstrap.css'
 import bootstrap from 'bootstrap/dist/js/bootstrap.bundle.js'
 
-createApp(Login).use(bootstrap).mount('#app')
+createApp(App).use(bootstrap).mount('#app')
