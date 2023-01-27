3573ef752dfc295dca2853994e52105f6fea8681
onrawin <onrawin.h@ku.th>
2023-01-27 10:53:29 +0700

    update url base

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import vue from '@vitejs/plugin-vue'
 import path from 'path'
 
 export default defineConfig({
-  base: '/test-arjs-troisjs/',
+  base: '/test-arjs-troisjs/.github.io/',
   plugins: [vue()],
   resolve: {
     alias: {
