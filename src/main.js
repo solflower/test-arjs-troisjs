35fa4a12081333d84917b930499b1801764685e0
onrawin <onrawin.h@ku.th>
2023-01-27 10:02:49 +0700

    reverse mainjs

--- src/main.js
+++ src/main.js
@@ -1,15 +1,9 @@
-import { createApp } from 'vue'
-import { TroisJSVuePlugin } from 'troisjs';
+import { createApp } from 'troisjs'
 import App from './App.vue'
+import testar from './views/testar.vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import bootstrap from 'bootstrap/dist/js/bootstrap.bundle.js'
 
 import router from '@/router'
 
-const app = createApp(App)
-
-app.use(router)
-app.use(bootstrap)
-app.use(TroisJSVuePlugin)
-app.mount('#app')
-//createApp(App).use(router,bootstrap).mount('#app')
+createApp(App).use(router,bootstrap).mount('#app')
