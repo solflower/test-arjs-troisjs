fb58fb442aa814d651145406317f25a8ed26509a
onrawin <onrawin.h@ku.th>
2023-01-27 11:55:41 +0700

    delete base url

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import vue from '@vitejs/plugin-vue'
 import path from 'path'
 
 export default defineConfig({
-  base: '/test-arjs-troisjs/',
+  base: '',
   plugins: [vue()],
   resolve: {
     alias: {
