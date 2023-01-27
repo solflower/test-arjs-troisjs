4d6a557ede54b93b62eb6811636d45f2799eff64
onrawin <onrawin.h@ku.th>
2023-01-27 09:26:23 +0700

    change mainjs detail

--- src/main.js
+++ src/main.js
@@ -1,8 +1,15 @@
-import { createApp } from 'troisjs'
+import { createApp } from 'vue'
+import { TroisJSVuePlugin } from 'troisjs';
 import App from './App.vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import bootstrap from 'bootstrap/dist/js/bootstrap.bundle.js'
 
 import router from '@/router'
 
-createApp(App).use(router,bootstrap).mount('#app')
+const app = createApp(App)
+
+app.use(router)
+app.use(bootstrap)
+app.use(TroisJSVuePlugin)
+app.mount('#app')
+//createApp(App).use(router,bootstrap).mount('#app')
